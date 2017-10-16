f76573e8ea8cc312b5c107170df9a548224e24fb
lifeiyan <1553652636@qq.com>
2017-10-16 18:38:26 +0800

    Initial Commit

---
+++ news/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  news
+//
+//  Created by 李飞艳 on 2017/10/16.
+//  Copyright © 2017年 李飞艳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ news/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  news
+//
+//  Created by 李飞艳 on 2017/10/16.
+//  Copyright © 2017年 李飞艳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
