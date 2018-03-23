d16a788c02cd81c2569914e6a760e67bfacd6968
lifeiyan <1553652636@qq.com>
2018-03-23 19:59:47 +0800

    加缓存

---
+++ news/LocalData.m
@@ -0,0 +1,13 @@
+//
+//  LocalData.m
+//  news
+//
+//  Created by 李飞艳 on 2018/3/13.
+//  Copyright © 2018年 李飞艳. All rights reserved.
+//
+
+#import "LocalData.h"
+
+@implementation LocalData
+
+@end
